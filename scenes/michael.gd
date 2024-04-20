3381d780b116162431ff9807d89fe0b2a90893f5
Anej <majnik2001@gmail.com>
2024-04-20 20:48:17 +0200

    Made scenes folder and moved game_level and character scene in it. AAdded character sprite and basic movement. Added character to main scene

---
+++ scenes/michael.gd
@@ -0,0 +1,16 @@
+extends CharacterBody2D
+
+@export var move_speed : float = 100
+
+func _physics_process(_delta):
+	# Get input direction
+	var input_direction = Vector2(
+		Input.get_action_strength("right") - Input.get_action_strength("left"),
+		Input.get_action_strength("down") - Input.get_action_strength("up")
+	)
+	
+	#Update velocity
+	velocity = input_direction * move_speed
+
+	#Move and slide function uses velocity of character body to move character
+	move_and_slide()
